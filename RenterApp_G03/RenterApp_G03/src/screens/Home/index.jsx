8b63cd9094cbb1adf3d7f75cf0ceb29c5614c0ad
HyunSeongLee <devhyun05@gmail.com>
2024-04-02 20:29:40 -0400

    added mapview

--- RenterApp_G03/RenterApp_G03/src/screens/Home/index.jsx
+++ RenterApp_G03/RenterApp_G03/src/screens/Home/index.jsx
@@ -1,20 +1,60 @@
-import React from 'react'; 
-import { View, Text, StyleSheet } from 'react-native'; 
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps" // Import PROVIDER_GOOGLE for Android
+import { StyleSheet, View, Platform } from "react-native"
+import * as Location from "expo-location"
 
 const Home = () => {
-    return (
-        <View style={styles.container}> 
-            <Text>This will be the search page</Text>
-        </View>
-    )
+  const [userLocation, setUserLocation] = useState(null) // Initialize state for user location
+
+  useEffect(() => {
+    const fetchLocation = async () => {
+      try {
+        let { status } = await Location.requestForegroundPermissionsAsync()
+        if (status === "granted") {
+          let location = await Location.getCurrentPositionAsync({})
+          setUserLocation(location.coords)
+          console.log(
+            "Location:",
+            location.coords.latitude,
+            location.coords.longitude
+          )
+        } else {
+          console.log("Location permission denied")
+        }
+      } catch (error) {
+        console.error("Error fetching location:", error)
+      }
+    }
+    fetchLocation()
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      {userLocation && (
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: userLocation.latitude,
+            longitude: userLocation.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      )}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
 })
-export default Home;
+
+export default Home
