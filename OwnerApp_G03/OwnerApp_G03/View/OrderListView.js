15a0c606137a509bbe57de6ebdaebea69c5af083
HyunSeongLee <devhyun05@gmail.com>
2024-04-04 19:07:14 -0400

    order detail, order list updated

--- OwnerApp_G03/OwnerApp_G03/View/OrderListView.js
+++ OwnerApp_G03/OwnerApp_G03/View/OrderListView.js
@@ -1,3 +1,6 @@
+import React, { useEffect, useState } from "react"
+import { auth, db } from "../firebaseConfig"
+import { collection, doc, getDoc, onSnapshot } from "firebase/firestore"
 import {
   FlatList,
   Pressable,
@@ -6,89 +9,95 @@ import {
   View,
   Image,
 } from "react-native"
-import React, { Component, useEffect, useState } from "react"
-import { auth, db } from "../firebaseConfig"
-import { select } from "../Controller/fireDBHelper"
-import { collection, doc, onSnapshot } from "firebase/firestore"
 import OrderImage from "../assets/electric-car.png"
+
+const CarItem = ({ item, onDetailPress }) => {
+  const [userName, setUserName] = useState("")
+
+  useEffect(() => {
+    const getUserName = async (user) => {
+      try {
+        const userDoc = await getDoc(doc(db, "Rentals", user))
+
+        setUserName(userDoc.data().name)
+      } catch (error) {
+        console.error("Error fetching user data:", error)
+      }
+    }
+
+    getUserName(item.rental)
+  }, [item.rental])
+
+  return (
+    <Pressable onPress={() => onDetailPress(item)} style={styles.item}>
+      <Image style={styles.itemImage} source={OrderImage} />
+      <Text style={styles.itemText}>{userName} sent request</Text>
+    </Pressable>
+  )
+}
+
 const OrderListView = ({ navigation, route }) => {
-  const renderCarItem = ({ item }) => {
-    return (
-      <Pressable onPress={() => onDetailPress(item)} style={styles.item}>
-        <Image style={styles.itemImage} source={OrderImage} />
-        <Text style={styles.itemText}>{item.rental} sent request</Text>
-      </Pressable>
+  const [orderList, setOrderList] = useState([])
+  const [user, setUser] = useState({})
+  const [userList, setUserList] = useState([])
+
+  useEffect(() => {
+    const updateUserList = () => {
+      const temp = orderList.filter((order) =>
+        user.orderList.includes(order.id)
+      )
+      setUserList(temp)
+    }
+
+    updateUserList()
+  }, [orderList, user])
+
+  useEffect(() => {
+    const unsubscribeUser = onSnapshot(
+      doc(db, "Owners", auth.currentUser.email),
+      (doc) => {
+        setUser(doc.data())
+      }
     )
-  }
+
+    return () => {
+      unsubscribeUser()
+    }
+  }, [])
+
+  useEffect(() => {
+    const unsubscribeOrders = onSnapshot(
+      collection(db, "Orders"),
+      (querySnapshot) => {
+        const temp = []
+        querySnapshot.forEach((doc) => {
+          const vehicle = {
+            id: doc.id,
+            ...doc.data(),
+          }
+          temp.push(vehicle)
+        })
+        setOrderList(temp)
+      }
+    )
+
+    return () => {
+      unsubscribeOrders()
+    }
+  }, [])
 
   const onDetailPress = (item) => {
     navigation.navigate("OrderDetail", { item: item })
   }
 
-  const [orderList, setOrderList] = useState([])
-
-  const [user, setUser] = useState({})
-  const [userList, setUserList] = useState([])
-
-  const updateUserList = () => {
-    const temp = orderList.filter((order) => user.orderList.includes(order.id))
-    setUserList(temp)
-  }
-
-  useEffect(() => {
-    updateUserList()
-  }, [orderList, user])
-
-  useEffect(() => {
-    try {
-      const unsubscribe = onSnapshot(
-        doc(db, "Owners", auth.currentUser.email),
-        (doc) => {
-          setUser(doc.data())
-        }
-      )
-
-      return () => {
-        unsubscribe()
-      }
-    } catch (err) {
-      console.error(err)
-    }
-  }, [])
-
-  useEffect(() => {
-    try {
-      const unsubscribe = onSnapshot(
-        collection(db, "Orders"),
-        (querySnapshot) => {
-          const temp = []
-          querySnapshot.forEach((doc) => {
-            const vehicle = {
-              id: doc.id,
-              ...doc.data(),
-            }
-            temp.push(vehicle)
-          })
-          setOrderList(temp)
-        }
-      )
-
-      return () => {
-        unsubscribe()
-      }
-    } catch (err) {
-      console.error(err)
-    }
-  }, [])
-
   return (
     <View style={styles.container}>
       <FlatList
         data={userList}
-        key={(item) => {
-          return item.id
-        }}
-        renderItem={(item) => renderCarItem(item)}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <CarItem item={item} onDetailPress={onDetailPress} />
+        )}
       />
     </View>
   )
